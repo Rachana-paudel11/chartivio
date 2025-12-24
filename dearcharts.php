287ece4e83cf03f368c35d1c9973690b2735b228
Rachana-paudel11 <rachanapaudel2061@gmail.com>
2025-12-24 14:11:40 +0530

    create header

---
+++ dearcharts.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: DearCharts
+ * Plugin URI: https://example.com/dearcharts
+ * Description: A basic WordPress plugin header for DearCharts.
+ * Version: 1.0.0
+ * Author: Rachana Paudel
+ * Author URI: https://example.com
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: dearcharts
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
